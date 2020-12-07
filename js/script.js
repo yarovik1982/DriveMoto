bbdba58fbefdb436f3aebcf1c5dc6ba828f84527
yarovik1982 <65862979+yarovik1982@users.noreply.github.com>
2020-12-07 12:47:19 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -29,4 +29,11 @@ $(function() {
         prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/arrow-left-black.svg" alt=""></button>',
         nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-right-black.svg" alt=""></button>',
     });
+
+    $('.filter-style').styler();
+
+    $('.filter__item-drop').on('click', function(){
+        $(this).toggleClass('filter__item-drop--active');
+        $(this).next().slideToggle('200');
+    });
 });
