c50255c6f30c6a233bfd5a67ac55de89890fc728
yarovik1982 <65862979+yarovik1982@users.noreply.github.com>
2020-12-11 00:25:43 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -25,15 +25,35 @@ $(function() {
 
     $('.product-slider').slick({
         slidesToShow: 4,
-        slidesToScroll: 1,
+        slidesToScroll: 3,
         prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/arrow-left-black.svg" alt=""></button>',
         nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-right-black.svg" alt=""></button>',
     });
 
     $('.filter-style').styler();
 
-    $('.filter__item-drop').on('click', function(){
+    $('.filter__item-drop,.filter__extra').on('click', function(){
         $(this).toggleClass('filter__item-drop--active');
         $(this).next().slideToggle('200');
+    });
+
+    $(".js-range-slider").ionRangeSlider({
+        type:"double",
+        grid:false,
+        min:100000,
+        max:500000,
+
+    });
+
+    $(".catalog__filter-btngrid").on("click", function() {
+        $(this).addClass("catalog__filter-button--active");
+        $(".catalog__filter-btnline").removeClass("catalog__filter-button--active");
+        $(".product-item__wrapper").removeClass("product-item__wrapper--list");
+    });
+
+    $(".catalog__filter-btnline").on("click", function() {
+        $(this).addClass("catalog__filter-button--active");
+        $(".catalog__filter-btngrid").removeClass("catalog__filter-button--active");
+        $(".product-item__wrapper").addClass("product-item__wrapper--list");
     });
 });
